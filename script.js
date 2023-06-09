c5f2df7cafd89e4793b346e59d2ed734cd5e02a9
Peter Ogada <peterogada@Peters-MacBook-Air-2.local>
2023-06-09 18:36:42 -0400

    Redid the todo app from scratch finished working on it for the day pushing up to github

--- script.js
+++ script.js
@@ -1,86 +1,34 @@
-const itemsContainer = document.querySelector('#items');
-const itemTemplate = document.querySelector('#itemTemplate');
+const ol = document.querySelector('ol');
+const input = document.querySelector('#item');
 const addButton = document.querySelector('#add');
+const deleteButton = document.querySelector('#delete');
 
-/*Next step is to fetch the existing items from localstorage, local storage is the way we achieve
-the refresh and the items still being there. So we can create a function that fetches the local storage 
-for existing items.*/
 
-let items = getItems();
+itemsArray = localStorage.getItem('items', ) ? JSON.parse(localStorage.getItem('items')) : [];
 
-function getItems() {
-  let value = localStorage.getItem('todo') || '[]' ;
 
-  return JSON.parse(value);
+itemsArray.forEach(listMaker); 
 
+function listMaker(text) {
+  const li = document.createElement('li');
+  li.textContent = text;
+  ol.appendChild(li);
 }
-
-/*The next function we need to define is to set the items once a user had added an item or changed an item we want to
-set the item back we want to refresh what we just saved*/
-
-function setItems(items) {
-  let itemJson = JSON.stringify(items);
-
-  localStorage.setItem('todo', itemJson);
-}
-
-/* We can now define the logic for adding a new item we use unshift or push to add an item at the front or back of the array
-because we want the new item to appear either at the top or bottom*/
-
-function addItem() {
-  items.push({
-    description: '',
-    completed: false
-  });
-
-  setItems(items);
-  refreshList();
-}
-
-function updateItem(item, key, value) {
-  item[key] = value;
-  setItems(items);
-  refreshList();
-}
-
-/*You want to refresh the list once the item has been added, the refresh list is going to take the list of items 
-the it is going to render that to the user */
-
-function refreshList() {
-  //TODO sort items
-
-  itemsContainer.textContent = '';
-
-  for (const item of items) {
-    const itemElement = itemTemplate.content.cloneNode(true);
-    const descriptionInput = itemElement.querySelector('.item-description');
-    const checkboxInput = itemElement.querySelector('.item-completed')
-
-    descriptionInput.value = item.description;
-    checkboxInput.checked = item.completed;
-
-    descriptionInput.addEventListener('change', function() {
-      updateItem(item, 'description', descriptionInput.value )
-    })
-
-    checkboxInput.addEventListener('change', function() {
-      updateItem(item, 'completed', checkboxInput.checked)
-    })
-
-    itemsContainer.append(itemElement);
-  }
-
-
-}
-
 
 addButton.addEventListener('click', function() {
-  addItem();
+  itemsArray.push(input.value);
+  localStorage.setItem('items', JSON.stringify(itemsArray));
+  listMaker(input.value);
+  input.value = '';
+})
+
+deleteButton.addEventListener('click', function() {
+  localStorage.clear();
+  ol.textContent = '';
+  itemsArray = [];
 })
 
 
-
-refreshList();
 
 
 
