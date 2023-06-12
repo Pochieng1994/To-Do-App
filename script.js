17f8ff78cf03afe247b8a9f7840a2e6ea1b8544d
Peter Ogada <peterogada@Peters-MacBook-Air-2.local>
2023-06-12 19:47:19 -0400

    edited script file pushing to guthub

--- script.js
+++ script.js
@@ -4,10 +4,56 @@ const addButton = document.querySelector('#add');
 const deleteButton = document.querySelector('#delete');
 
 
-itemsArray = localStorage.getItem('items', ) ? JSON.parse(localStorage.getItem('items')) : [];
+
+todoItems = localStorage.getItem('items') ? JSON.parse(localStorage.getItem('items')) : [];
+
+
+function addTodo() {
+  let textInput = input.value;
+
+  const todo = {
+    text: textInput,
+    id: Date.now(),
+  }
+
+  todoItems.push(todo);
+  console.log(todoItems)
+  setItem();
+  listMaker(textInput);
+  input.value = '';
+}
+
+function setItem() {
+  let stringifiedObj = JSON.stringify(todoItems);
+  localStorage.setItem('text', stringifiedObj);
+}
+
+function listMaker(text) {
+  const li = document.createElement('li');
+  li.textContent = text;
+  ol.appendChild(li);
+}
+
+
+addButton.addEventListener('click', function() {
+  
+  addTodo()
+})
+
+
+
+/*Now we are going to create an array where were going to store all of the items on our todo list we have 
+to check and see whether or not were using local storage if we are then we are going to initialize our array
+w/ the data thats already in our local storage. If were not using local storage then were going to create a 
+new array*/
+
+/*itemsArray = localStorage.getItem('items', ) ? JSON.parse(localStorage.getItem('items')) : [];
 
 
 itemsArray.forEach(listMaker); 
+
+ Now we have to place the data within our items array on our screen
+
 
 function listMaker(text) {
   const li = document.createElement('li');
@@ -26,7 +72,7 @@ deleteButton.addEventListener('click', function() {
   localStorage.clear();
   ol.textContent = '';
   itemsArray = [];
-})
+})*/
 
 
 
